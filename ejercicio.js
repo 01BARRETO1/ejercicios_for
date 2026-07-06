8b011020e8bbd85443e54113fca01544f379295b
01BARRETO1 <karbar_1990@hotmail.com>
2026-07-06 13:47:46 -0500

    Reto 1 - depuracion de for

--- ejercicio.js
+++ ejercicio.js
@@ -11,6 +11,9 @@ function ejecutar(numEjercicio) {
         listarNumerosReversa();
     } else if (numEjercicio == 3) {
         listarPares();
+    }else if(numEjercicio==4){
+        listarImpares();
+
     }
 
 }
@@ -26,6 +29,15 @@ function listarNumerosReversa() {
 //ejercicio 3, lsitar pares
 function listarPares() {
     for (let i = 0; i < 10; i += 2) {
+        console.log(i);
+    }
+}
+
+//reto for
+/*Crear una función llamada listarImpares, que imprima en consola los
+números impares desde el 1 hasta el 7*/
+function listarImpares(){
+    for(i=1; i<=7; i+=2){
         console.log(i);
     }
 }
